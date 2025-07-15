72e011a4d26c869aa6b1e8513c36785b41499326
hun9812 <108339136+hun9812@users.noreply.github.com>
2025-07-15 17:13:20 +0900

    Time: 485 ms (95.66%), Space: 0B (100%) - LeetHub

---
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -0,0 +1,8 @@
+# Write you r MySQL query statement below
+SELECT name as Customers
+FROM Customers
+WHERE NOT EXISTS (
+    SELECT customerId
+    FROM Orders
+    WHERE Orders.customerId = Customers.id 
+)
