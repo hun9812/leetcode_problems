533f593f4ec55a43595ac9feb67357c32103fff9
hun9812 <108339136+hun9812@users.noreply.github.com>
2025-08-28 21:30:53 +0900

    Time: 0 ms (100%), Space: 17.8 MB (48.52%) - LeetHub

---
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findLUSlength(self, a: str, b: str) -> int:
+        if len(a) != len(b):
+            return max(len(a), len(b))
+        
+        if a == b:
+            return -1
+        else:
+            return len(a)
+        
