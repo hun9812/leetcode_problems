8a1375da4edcbb9813f5e0308e5f285293dcde8c
hun9812 <108339136+hun9812@users.noreply.github.com>
2025-09-25 18:30:30 +0900

    Time: 0 ms (100%), Space: 17.7 MB (51.59%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        res = 0
+        for i in stones:
+            if i in jewels:
+                res += 1
+        return res
