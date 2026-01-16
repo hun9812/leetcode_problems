fb95e27eed8fdf598cca753cca132ba5a148153e
hun9812 <108339136+hun9812@users.noreply.github.com>
2026-01-16 22:57:46 +0900

    Time: 787 ms (67.45%), Space: 0B (100%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+-- DELETE p1 FROM Person p1, Person p2
+-- WHERE p1.email = p2.email and p1.id > p2.id
+
+DELETe P1
+FROM Person AS P1, Person AS P2
+WHERE P1.email = P2.email and P1.id > P2.id
