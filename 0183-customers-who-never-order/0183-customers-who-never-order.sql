362114df97c8f0a22bf008bc6260e6b135143a39
hun9812 <108339136+hun9812@users.noreply.github.com>
2026-01-16 22:55:28 +0900

    Time: 541 ms (39.86%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -7,9 +7,17 @@
 --     WHERE Orders.customerId = Customers.id 
 -- )
 
-SELECT C.name AS Customers
-FROM Customers AS C
-LEFT OUTER JOIN Orders AS O
-ON C.id = O.customerId
-GROUP BY C.id
-HAVING COUNT(customerId) = 0
+-- SELECT C.name AS Customers
+-- FROM Customers AS C
+-- LEFT OUTER JOIN Orders AS O
+-- ON C.id = O.customerId
+-- GROUP BY C.id
+-- HAVING COUNT(customerId) = 0
+
+SELECT name AS Customers
+FROM Customers C
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM Orders O 
+    WHERE O.customerId = C.id
+);
