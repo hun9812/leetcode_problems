023f48785f30c0b3a93bff262bbdefc0f1c3c93f
hun9812 <108339136+hun9812@users.noreply.github.com>
2026-01-16 22:45:05 +0900

    Time: 527 ms (50.46%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,8 +1,15 @@
 # Write you r MySQL query statement below
-SELECT name as Customers
-FROM Customers
-WHERE NOT EXISTS (
-    SELECT customerId
-    FROM Orders
-    WHERE Orders.customerId = Customers.id 
-)
+-- SELECT name as Customers
+-- FROM Customers
+-- WHERE NOT EXISTS (
+--     SELECT customerId
+--     FROM Orders
+--     WHERE Orders.customerId = Customers.id 
+-- )
+
+SELECT C.name AS Customers
+FROM Customers AS C
+LEFT OUTER JOIN Orders AS O
+ON C.id = O.customerId
+GROUP BY C.id
+HAVING COUNT(customerId) = 0
