0f170ff658f6c301d3d46aa724ed6f0df3f5f70f
hun9812 <108339136+hun9812@users.noreply.github.com>
2026-01-29 19:24:44 +0900

    Time: 559 ms (8.1%), Space: 0B (100%) - LeetHub

--- 3465-find-products-with-valid-serial-numbers/3465-find-products-with-valid-serial-numbers.sql
+++ 3465-find-products-with-valid-serial-numbers/3465-find-products-with-valid-serial-numbers.sql
@@ -1,8 +1,12 @@
 # Write your MySQL query statement below
-select *
-from products
-where REGEXP_LIKE(description, " SN[0-9]{4}\\-[0-9]{4} ", 'c') 
-or REGEXP_LIKE(description, " SN[0-9]{4}\\-[0-9]{4}$", 'c')
-or REGEXP_LIKE(description, "^SN[0-9]{4}\\-[0-9]{4} ", 'c')
-or REGEXP_LIKE(description, "^SN[0-9]{4}\\-[0-9]{4}$", 'c')
-order by product_id
+SELECT *
+FROM products
+WHERE REGEXP_LIKE(description, '\\bSN[0-9]{4}-[0-9]{4}\\b', 'c')
+ORDER BY product_id;
+-- select *
+-- from products
+-- where REGEXP_LIKE(description, " SN[0-9]{4}\\-[0-9]{4} ", 'c') 
+-- or REGEXP_LIKE(description, " SN[0-9]{4}\\-[0-9]{4}$", 'c')
+-- or REGEXP_LIKE(description, "^SN[0-9]{4}\\-[0-9]{4} ", 'c')
+-- or REGEXP_LIKE(description, "^SN[0-9]{4}\\-[0-9]{4}$", 'c')
+-- order by product_id
