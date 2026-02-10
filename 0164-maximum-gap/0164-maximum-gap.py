a6029bda6d1d5f0c96e4666054ac7097b7f90a35
hun9812 <108339136+hun9812@users.noreply.github.com>
2026-02-10 16:43:09 +0900

    Time: 275 ms (32.21%), Space: 34.3 MB (20.41%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -1,11 +1,46 @@
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
+        # pigeonhole principle
         if len(nums) == 1:
             return 0
+        min_num = nums[0]
+        max_num = nums[0]
+        for i in range(1, len(nums)):
+            min_num = min(min_num, nums[i])
+            max_num = max(max_num, nums[i])
+
+        if min_num == max_num:
+            return 0
+        size = max(1, (max_num - min_num) // (len(nums)-1))
+        bucket = [[None, None] for _ in range((max_num - min_num) // size + 1)]
+
+        for num in nums:
+            idx = (num - min_num) // size
+            if bucket[idx][0] is None:
+                bucket[idx][0] = num
+                bucket[idx][1] = num
+            else:
+                bucket[idx][0] = min(bucket[idx][0], num)
+                bucket[idx][1] = max(bucket[idx][1], num)
         
-        nums.sort()
-        res = nums[1]-nums[0]
-        for i in range(2, len(nums)):
-            res = max(res, nums[i]-nums[i-1])
+        max_size = 0
+        prev_max = min_num
+
+        for b in bucket:
+            if b[0] is not None:
+                max_size = max(max_size, b[0] - prev_max)
+                prev_max = b[1]
         
-        return res
+        return max_size
+        
+        
+        
+        # if len(nums) == 1:
+        #     return 0
+        
+        # nums.sort()
+        # res = nums[1]-nums[0]
+        # for i in range(2, len(nums)):
+        #     res = max(res, nums[i]-nums[i-1])
+        
+        # return res
